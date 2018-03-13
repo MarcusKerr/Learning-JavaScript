474958e462a3da3e0182277f4437b638710e8887
Marcus <marcus@Iris-Ver32M.connect>
2018-03-13 16:18:09 +0000

    Learning the difference between strings and numbers

--- HelloUniverse/script.js
+++ HelloUniverse/script.js
@@ -4,4 +4,12 @@ let myName = "Marcus",
     middleInitial = "A",
     lastInitial = "K";
 
+let price = 2,
+    price2 = 4.67,
+    price3 = "5";
+
 console.log(myName, middleInitial, lastInitial);
+
+console.log(price, price2, price + price2, price + price3);
+
+console.log(myName + " " + middleInitial);
