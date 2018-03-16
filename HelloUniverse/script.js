263368affaaa3e1133ce2c4e1d6889dc80b0563e
Marcus <marcus@Iris-Ver32M.local>
2018-03-16 15:47:32 +0000

    Simple aritmetic operations

--- HelloUniverse/script.js
+++ HelloUniverse/script.js
@@ -4,12 +4,22 @@ let myName = "Marcus",
     middleInitial = "A",
     lastInitial = "K";
 
-let price = 2,
+console.log(myName, middleInitial, lastInitial);
+console.log(myName + " " + middleInitial);
+
+    let price = 2,
     price2 = 4.67,
     price3 = "5";
 
-console.log(myName, middleInitial, lastInitial);
-
 console.log(price, price2, price + price2, price + price3);
 
-console.log(myName + " " + middleInitial);
+
+let first = 50,
+    second = 20;
+
+let answer = first + second;
+
+cosnole.log(answer);
+
+
+
