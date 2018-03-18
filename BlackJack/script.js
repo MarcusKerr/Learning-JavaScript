d1d76e3c8990aec693d69213110599bbbc3c419a
Marcus <marcus@Iris-Ver32M.local>
2018-03-18 14:31:33 +0000

    Created card variables

---
+++ BlackJack/script.js
@@ -0,0 +1,11 @@
+/* 
+    BlackJack tutorial
+    Marcus Kerr
+*/
+
+// Card variable 
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+//print cards to console
+console.log("Game start, you were dealt:\n\t\t\t" + card1 + "\n\t\t\t" + card2);
