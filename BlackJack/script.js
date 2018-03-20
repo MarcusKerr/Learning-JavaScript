b5b8203030b37db7334ff803ed355a60268cde32
Marcus <marcus@Iris-Ver32M.local>
2018-03-20 15:13:14 +0000

    Added functions to create deck and to get the next card

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -9,41 +9,42 @@ let suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
 //card values array 
 let values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']; 
 
-//deck of cards array
-let deck = [];
+//function to create deck of cards
+function createDeck () {
 
-//for loop to create deck of cards
+    //clear deck
+     let deck = []; 
 
-//loop through each suit
-/*for(suitIdx = 0; suitIdx<suits.length; suitIdx++){
+    //for loop to create deck of cards
+    //loop through each suit
+    for(suitIdx = 0; suitIdx<suits.length; suitIdx++){
 
-    //For each suit, loop through each value
-    for(valuesIdx = 0; valuesIdx<values.length; valuesIdx++){
-         
-        //add card (combination of suit and value) to deck array
-        deck.push(values[valuesIdx] + " of " + suits[suitIdx]);
+        //For each suit, loop through each value
+        for(valuesIdx = 0; valuesIdx<values.length; valuesIdx++){
+            
+            //add card (combination of suit and value) to deck array
+            deck.push(values[valuesIdx] + " of " + suits[suitIdx]);
+        }
     }
-}*/
-
-
-//While loop equivalent of for loop
-suitIdx = 0
-while (suitIdx<suits.length){
-
-    valuesIdx = 0;
-    while (valuesIdx<values.length){
-        deck.push(values[valuesIdx] + " ox " + suits[suitIdx]);
-        valuesIdx ++;
-    }
-    suitIdx ++
+    return deck;
 }
+
+//function to get card from top of the deck
+function getNextCard(){
+    return deck.shift();
+}
+
+
+//Call deck function to create deck of cards 
+let deck = createDeck();
+
 //for loop to check that deck was created
-for(i=0; i<deck.length; i++){
-    console.log(deck[i]);
-}
+// for(i=0; i<deck.length; i++){
+//     console.log(deck[i]);
+// }
 
 //players can have two or more cards so best to use an array 
-let playerCards = [deck[0], deck[2]];
+let playerCards = [getNextCard(), getNextCard()];
  
 
 //print cards to console
