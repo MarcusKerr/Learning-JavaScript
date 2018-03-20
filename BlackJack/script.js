35c7fc9198cec4b493a596234d36122c5c4197fb
Marcus <marcus@Iris-Ver32M.local>
2018-03-20 16:30:56 +0000

    Created card object

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -17,18 +17,29 @@ function createDeck () {
 
     //for loop to create deck of cards
     //loop through each suit
-    for(suitIdx = 0; suitIdx<suits.length; suitIdx++){
+    for(suitsIdx = 0; suitsIdx<suits.length; suitsIdx++){
 
         //For each suit, loop through each value
         for(valuesIdx = 0; valuesIdx<values.length; valuesIdx++){
+
+            // create card ob ject
+            let card =  { 
+                suit: suits[suitsIdx],
+                value: values[valuesIdx]
+
+            };
             
-            //add card (combination of suit and value) to deck array
-            deck.push(values[valuesIdx] + " of " + suits[suitIdx]);
+            //puch card object to deck array  
+            deck.push(card);
         }
     }
     return deck;
 }
 
+//function to print out card 
+function getCardString(card){
+    return card.value + " of " + card.suit;
+}
 //function to get card from top of the deck
 function getNextCard(){
     return deck.shift();
@@ -38,14 +49,8 @@ function getNextCard(){
 //Call deck function to create deck of cards 
 let deck = createDeck();
 
-//for loop to check that deck was created
-// for(i=0; i<deck.length; i++){
-//     console.log(deck[i]);
-// }
-
 //players can have two or more cards so best to use an array 
 let playerCards = [getNextCard(), getNextCard()];
  
-
 //print cards to console
-console.log("Game start,\nYou were dealt:\n\t\t\t" + playerCards[0] + "\n\t\t\t" + playerCards[1]);
+console.log("Game start,\nYou were dealt:\n\t\t\t" + getCardString(playerCards[0]) + "\n\t\t\t" + getCardString(playerCards[1]));
