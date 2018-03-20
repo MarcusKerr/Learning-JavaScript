20cc3e1cc9fd35d61e8fdd74697461c671fe91b6
Marcus <marcus@Iris-Ver32M.local>
2018-03-20 13:57:15 +0000

    Used for loops and arrays to create a deck of cards

--- BlackJack/script.js
+++ BlackJack/script.js
@@ -3,11 +3,30 @@
     Marcus Kerr
 */
 
-//cards array
-let deck = ["Ace of Spades",
-            "Two of Spades",
-            "Three of Spades"
-    ];
+//suit array
+let suits = ['Spades', 'Diamonds', 'Hearts', 'Clubs'];
+
+//card values array 
+let values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']; 
+
+//deck of cards array
+let deck = [];
+
+//for loop to create deck of cards
+for(suitIdx = 0; suitIdx<suits.length; suitIdx++){
+
+    //Nested for loop to loop through values
+    for(valuesIdx = 0; valuesIdx<values.length; valuesIdx++){
+         
+        //add cards to deck array
+        deck.push(values[valuesIdx] + " of " + suits[suitIdx]);
+    }
+}
+
+//for loop to check that deck was created
+for(i=0; i<deck.length; i++){
+    console.log(deck[i]);
+}
 
 //players can have two or more cards so best to use an array 
 let playerCards = [deck[0], deck[2]];
